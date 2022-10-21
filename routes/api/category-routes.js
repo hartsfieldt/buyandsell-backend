5a32fc338b1f975640fa8f1f2d3e37f0b7b94a23
Your Name <you@example.com>
2022-10-21 17:08:24 -0400

    created routes for fetching a single category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -19,6 +19,23 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: { id: req.params.id },
+    include: { model: Product },
+  })
+    .then((dbCategoryData) => {
+      if (!dbCategoryData) {
+        res
+          .status(404)
+          .json({ message: "No categories were found with this id." });
+        return;
+      }
+      res.status(200).json(dbCategoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post("/", (req, res) => {
